01a5549675b624df91d34fda87086426ac7d83eb
Asif Mohammed <asifm2882@gmail.com>
2020-05-04 01:28:41 +0530

    add production env configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDNWYsB_gpUjID9DzzcAAc2_nMW2TzP8YU',
+    authDomain: 'myrecipe-app.firebaseapp.com',
+    databaseURL: 'https://myrecipe-app.firebaseio.com',
+    projectId: 'myrecipe-app',
+    storageBucket: 'myrecipe-app.appspot.com',
+    messagingSenderId: '613879500925',
+  },
 };
