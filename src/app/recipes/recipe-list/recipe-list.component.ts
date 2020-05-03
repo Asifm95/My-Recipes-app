49ab84e9b0de2ade95da7ecd8df4ba93bb8ebb5f
Asif Mohammed <asifm2882@gmail.com>
2020-05-03 17:15:02 +0530

    code cleanup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,32 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from "../recipe.model";
-import { RecipeService } from "../recipe.service";
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Observable<Recipe[]>;
 
-
-  // recipes:Recipe[];
-  recipes: Observable<Recipe[]>
-
-  constructor(private recipeService:RecipeService) { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
-      // this.recipeService.recipeChanged
-      // .subscribe( (recipes: Recipe[]) => {
-      //     this.recipes = recipes;
-      // })
-      // this.recipes = this.recipeService.getData();
-
-      this.recipes = this.recipeService.getData()
-      // console.log(this);
-      
+    this.recipes = this.recipeService.getData();
   }
-
-
 }
